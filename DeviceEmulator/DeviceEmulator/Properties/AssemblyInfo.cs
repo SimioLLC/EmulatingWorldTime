652e13e818996f5341f8e6dd290a321dd8cac20f
batpox <dan.houck@batpox.com>
2023-03-13 06:27:20 -0400

    Updated to .NET Framework 4.7.2

--- DeviceEmulator/DeviceEmulator/Properties/AssemblyInfo.cs
+++ DeviceEmulator/DeviceEmulator/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simio LLC")]
 [assembly: AssemblyProduct("DeviceEmulator")]
-[assembly: AssemblyCopyright("Copyright ©  2018-2019")]
+[assembly: AssemblyCopyright("Copyright ©  2018-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("19.1.2.0")]
-[assembly: AssemblyFileVersion("19.1.2.0")]
+[assembly: AssemblyVersion("23.3.11.0")]
+[assembly: AssemblyFileVersion("23.3.11.0")]
